742eccfb13fc6948a3f9d39e8c119c809ebe9a2b
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2023-12-07 14:11:18 +0530

    Added Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+
+
 
 function Card() {
     return (
         <div>
-            <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'>
+            <div className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+                <FaRegFileAlt />
+                <p className='text-xs mt-5 font-semibold leading-tight'>Reprehenderit aliqua incididunt.</p>
+                <div className="footer absolute bottom-0 w-full left-0 " >
+                    <div className='flex items-center justify-between px-8 py-3 mb-3'>
+                        <h5>0.5 mb</h5>
+                        <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
+                            <LuDownload size=".8em" color='#fff' />
+                        </span>
+                    </div>
+                    <div className='tag w-full py-4 bg-green-600 flex justify-center items-center'>
+                        <h3 className='text-sm font-semibold'>
+                            Download Now
+                        </h3>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
